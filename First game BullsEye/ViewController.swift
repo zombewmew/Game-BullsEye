c55d52d5cc2afe0f4e8f777e8dd14e2e1ff828af
christina <blueberrrycake@gmail.com>
2019-09-12 13:14:55 +0300

    Initial Commit

---
+++ First game BullsEye/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  First game BullsEye
+//
+//  Created by zombewmew on 12.09.2019.
+//  Copyright © 2019 zombewmew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
